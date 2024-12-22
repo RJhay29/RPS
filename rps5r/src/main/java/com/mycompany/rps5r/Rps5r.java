2192c7eb099514f26b34c6a4684eb44addb8bb50
RJhay29 <jhayceryt@gmail.com>
2024-12-22 17:29:02 +0800

    Add files via upload

---
+++ rps5r/src/main/java/com/mycompany/rps5r/Rps5r.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.rps5r;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Rps5r {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
